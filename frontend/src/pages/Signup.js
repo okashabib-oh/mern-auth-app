51ca12df0a63f78aa2777dbaedac1e41aaac2aaa
Okasha Habib <160734391+okashabib-oh@users.noreply.github.com>
2024-10-18 01:02:21 +0500

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -30,7 +30,7 @@ function Signup() {
         }
 
         try {
-            const url = "http://localhost:8080/auth/signup"
+            const url = "https://mern-auth-app-rho-api.vercel.app/auth/signup"
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
